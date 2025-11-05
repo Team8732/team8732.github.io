3a3c6d2f4829d03f44093452f8a08ae56bd56f82
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-05 01:24:48 -0600

    made the navbar responsive!

--- team-site/src/components/Navbar.js
+++ team-site/src/components/Navbar.js
@@ -1,26 +1,39 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import './Navbar.css'
+import { FaBars } from "react-icons/fa";
 
 
 function Navbar() {
+
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = ()=> {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <>
         <nav className="navbar">
-            <div className="navbar-container">
-                <Link to="/" className="navbar-logo">
+            <Link to="/" className="navbar-logo">
                     <img src="/favicon.ico" alt="Logo" style={{ height: '60px' }} />
-                </Link>
+            </Link>
+
+            <div className= {isOpen ? "navbar-container active" : "navbar-container"}>
+                
 
                 <Link to="/about">About</Link>
                 <Link to="/team">Team</Link>
                 <Link to="/contact">Contact</Link>
                 <Link to="/test">Testing</Link>
 
-                <div className="menu-icon">
-
-                </div>
             </div>
+
+            <div className="menu-icon" onClick={toggleMenu}>
+                <FaBars/>
+            </div>
+        
         </nav>
     </>
   )
