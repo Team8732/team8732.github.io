7e8e0fbea31b691a999ed2aaf2ed40d132e8ab60
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-04 19:01:30 -0600

    building the navbar!

--- team-site/src/components/Navbar.js
+++ team-site/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
+import './Navbar.css'
 
 
 function Navbar() {
@@ -10,6 +11,10 @@ function Navbar() {
                 <Link to="/" className="navbar-logo">
                     <img src="/favicon.ico" alt="Logo" style={{ height: '40px' }} />
                 </Link>
+
+                <Link to="/about">About</Link>
+                <Link to="/team">Team</Link>
+                <Link to="/contact">Contact</Link>
 
                 <div className="menu-icon">
 
