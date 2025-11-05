f168c296e5aaaf7c1d7f42f825680fa38a9d4006
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-04 19:22:13 -0600

    finished making base of navbar

--- team-site/src/components/Navbar.js
+++ team-site/src/components/Navbar.js
@@ -9,12 +9,13 @@ function Navbar() {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">
-                    <img src="/favicon.ico" alt="Logo" style={{ height: '40px' }} />
+                    <img src="/favicon.ico" alt="Logo" style={{ height: '60px' }} />
                 </Link>
 
                 <Link to="/about">About</Link>
                 <Link to="/team">Team</Link>
                 <Link to="/contact">Contact</Link>
+                <Link to="/test">Testing</Link>
 
                 <div className="menu-icon">
 
