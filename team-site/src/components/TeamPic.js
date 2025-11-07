ca98973a538fdd4683f9cec6faf59f9c55341ffc
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-06 21:28:12 -0600

    scroll interaction on home page

--- team-site/src/components/TeamPic.js
+++ team-site/src/components/TeamPic.js
@@ -1,10 +1,37 @@
-import React from 'react'
-import './TeamPic.css'
+import React, { useEffect, useState } from 'react';
+import './TeamPic.css';
 
 export default function TeamPic() {
+  const [gateState, setGateState] = useState('initial');
+
+  useEffect(() => {
+
+    const handleScrollAttempt = () => {
+      if (gateState === 'initial') {
+        setGateState('revealed');
+
+        setTimeout(() => {
+          setGateState('unlocked');
+        }, 1000); 
+      }
+    };
+
+    window.addEventListener('wheel', handleScrollAttempt, { once: true });
+    window.addEventListener('touchstart', handleScrollAttempt, { once: true });
+
+    return () => {
+      window.removeEventListener('wheel', handleScrollAttempt);
+      window.removeEventListener('touchstart', handleScrollAttempt);
+    };
+  }, [gateState]);
+
   return (
-    <div className="team-container">
-        <img src='/images/team_image.JPG' alt="a lovely picture of my lovely team"></img>
+    <div className={`team-container ${gateState}`}>
+      <img src='/images/team_image.JPG' alt="a lovely picture of my lovely team" />
+      <div className="team-message">
+        <h2>Team 8732:</h2>
+        <h1>Trinity Force</h1>
+      </div>
     </div>
-  )
+  );
 }
