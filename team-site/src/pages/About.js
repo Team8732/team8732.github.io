2cee8254a342b899f054275dba4e99a950d2ca16
Shreya <153142441+shreya-0718@users.noreply.github.com>
2025-11-18 18:15:07 -0600

    added para tabs on about page

--- team-site/src/pages/About.js
+++ team-site/src/pages/About.js
@@ -11,12 +11,12 @@ function About() {
         </Hero>
 
         <TextSection>
-          <h1>Who are we?</h1>
-          <p>
+          <h1>Who are we?</h1>  
+          <p className="paragraph">
             Team 8732, Trinity Force, was founded to empower young women in STEM, creating a supportive space where underrepresented voices can grow, contribute, and lead. As an all-girls team, we intentionally focus on providing hands-on technical skills and real-world problem solving opportunities that allow young women, many from underrepresented backgrounds, to thrive in STEM fields traditionally lacking diversity.
-            <br/>
+          </p><p className="paragraph">
             We also value diversity beyond gender, actively engaging with local schools, churches, and community organizations to share our program and recruit students from all backgrounds. We seek to break down barriers to participation by ensuring that financial limitations never prevent students from joining.
-            <br/> 
+          </p><p className="paragraph">
             Within our team, we celebrate the unique strengths that come from each individual's perspective, knowing that these diverse ideas and ways of thinking help us approach problems creatively. By fostering this inclusive, supportive environment, our students learn the values of collaboration and mutual respect- skills that will benefit them well beyond the program. 
           </p>
         </TextSection>
